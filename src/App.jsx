39b10961f9abafbfc79d5c0a714d68f912d589ef
shivam-cantire <100751984+shivam-cantire@users.noreply.github.com>
2023-02-20 17:35:54 +0530

    main branch updated with base code

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,5 @@
-import Header from './components/Header/Header';
-import Courses from './components/Courses/Courses';
-import CreateCourse from './components/CreateCourse/CreateCourse';
-import { mockedCoursesList, mockedAuthorsList } from './constants/contants';
-import styles from './App.module.css';
-import React from 'react';
 function App() {
-	const [showCreateCourse, setShowCreateCourse] = React.useState(false);
-	const [showCourses, setShowCourses] = React.useState(true);
-	const [courseList, setCoursesList] = React.useState(mockedCoursesList);
-	const [authorsList, setAthorsList] = React.useState(mockedAuthorsList);
-	const handleShowHideCourse = (showCreate) => {
-		if (showCreate) {
-			setShowCreateCourse(true);
-			setShowCourses(false);
-		} else {
-			setShowCreateCourse(false);
-			setShowCourses(true);
-		}
-	};
-	const addNewCourses = (newCourses, newAuthors) => {
-		setAthorsList([...authorsList, ...newAuthors]);
-		setCoursesList([...courseList, ...newCourses]);
-	};
-	return (
-		<div className={styles.container}>
-			<Header />
-			<Courses
-				courses={courseList}
-				authors={authorsList}
-				onCreateCourse={handleShowHideCourse}
-				display={showCourses}
-			/>
-			<CreateCourse
-				appendNewCourses={addNewCourses}
-				onSubmitCreateCourse={handleShowHideCourse}
-				display={showCreateCourse}
-			/>
-		</div>
-	);
+	return <div>React</div>;
 }
 
 export default App;
--- src/components/Header/components/Logo/Logo.jsx
+++
@@ -1,4 +0,0 @@
-import onlineCourse from '../../../../assets/onlineCourse.png';
-export default function Logo() {
-	return <img alt='' style={{ height: '50px' }} src={onlineCourse}></img>;
-}
